1a742bfd3485dea993c18c3e29d5e7f43b2e60a9
favianca <favian@kargo.tech>
2023-05-26 14:40:37 +0700

    Fixed worker error catch

--- src/jobs/worker.ts
+++ src/jobs/worker.ts
@@ -51,70 +51,65 @@ async function getInbox() {
       },
     },
   });
-
-  await gmailClient.users.messages
-    .list({
+  try {
+    const response = await gmailClient.users.messages.list({
       userId: 'me',
       q: generateGmailQueryString(
         allSenderEmails.map((i) => i.email),
         new Date(),
         ['invoice', 'receipt', ...additionalKeywords],
       ),
-    })
-    .then((response) => {
-      // Construct an array of requests to retrieve the contents of each message
-      if (!response.data.messages.length) return Promise.reject('no-new-mail');
-      const requests = response.data.messages.map((message) => {
-        return gmailClient.users.messages.get({
-          userId: 'me',
-          id: message.id,
-        });
-      });
-      return Promise.all(requests);
-    })
-    .then((responses) => {
-      // Iterate through the responses and print the contents of each email
-      responses.forEach(({ data }) => {
-        const invoice = parseEmailsForInvoiceAmounts(data, currency);
-        let sender;
-        if (invoice) {
-          sender = allSenderEmails.find(
-            (email) => invoice.senderEmailAddress === email.email,
-          );
-          // delete because only sender id is needed
-          delete invoice.senderEmailAddress;
-          result.push({
-            currency,
-            senderEmail: {
-              connect: {
-                id: sender.id,
-              },
-            },
-            isValid: true,
-            user: {
-              connect: {
-                id: userId,
-              },
-            },
-            ...(sender.categoryId
-              ? {
-                  category: {
-                    connect: {
-                      id: sender.categoryId,
-                    },
-                  },
-                }
-              : {}),
-            created: new Date(),
-            ...invoice,
-          });
-        }
-      });
-    })
-    .catch((err) => {
-      if (err === 'no-new-mail') return Promise.reject('No new mail detected');
-      else return Promise.reject(err);
     });
+    // Construct an array of requests to retrieve the contents of each message
+    if (!response.data.messages.length) return Promise.reject('no-new-mail');
+    const requests = response.data.messages.map((message) => {
+      return gmailClient.users.messages.get({
+        userId: 'me',
+        id: message.id,
+      });
+    });
+    const responses = await Promise.all(requests);
+    // Iterate through the responses and print the contents of each email
+    responses.forEach(({ data }) => {
+      const invoice = parseEmailsForInvoiceAmounts(data, currency);
+      let sender;
+      if (invoice) {
+        sender = allSenderEmails.find(
+          (email) => invoice.senderEmailAddress === email.email,
+        );
+        // delete because only sender id is needed
+        delete invoice.senderEmailAddress;
+        result.push({
+          currency,
+          senderEmail: {
+            connect: {
+              id: sender.id,
+            },
+          },
+          isValid: true,
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+          ...(sender.categoryId
+            ? {
+                category: {
+                  connect: {
+                    id: sender.categoryId,
+                  },
+                },
+              }
+            : {}),
+          created: new Date(),
+          ...invoice,
+        });
+      }
+    });
+  } catch (err) {
+    if (err === 'no-new-mail') return Promise.reject('No new mail detected');
+    else return Promise.reject(err);
+  }
   await Promise.all(
     result.map((invoice) => prisma.invoice.create({ data: invoice })),
   );
